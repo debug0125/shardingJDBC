4fe0e2957c7335ef36ab4dc48e0f46cd6ae5fb44
renyuefeng <ryfds123git>
2020-07-09 16:45:34 +0800

    sql文件

---
+++ src/dbSql.sql
@@ -0,0 +1,31 @@
+-- 创建订单库order_db_1, order_db_2
+CREATE DATABASE `order_db_1` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+CREATE DATABASE `order_db_2` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+-- 分别在两个库中创建3张订单表
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1`  (
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+  `user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+  `status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `t_order_2`;
+CREATE TABLE `t_order_2`  (
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+  `user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+  `status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `t_order_3`;
+CREATE TABLE `t_order_3`  (
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+  `user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+  `status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
